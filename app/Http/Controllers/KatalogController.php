39de5a6d19944b34d54b6e199da6357b94f34808
ramadhani151221 <202410101050@mail.unej.ac.id>
2022-04-03 22:37:50 +0700

    Project setup baru, karena yang lama error

---
+++ app/Http/Controllers/KatalogController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use Illuminate\Support\Str;
+
+class KatalogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $product = \App\Models\Product::all();
+        return view('product.index',compact('product'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $category = \App\Models\Category::all();
+        return view('product.create',compact('category'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $dataValidator = [
+            'category_id' => 'required|numeric',
+            'name' => 'required|string',
+            'description' => 'required|string',
+        ];
+        $validator = Validator::make($input,$dataValidator);
+        if($validator->fails()){
+            return back()->with('error', $validator->errors()->all());
+        }
+
+        $dataCreate = [
+            'category_id' => $request->category_id,
+            'name' => $request->name,
+            'description' => $request->description,
+        ];
+        $product = \App\Models\Product::create($dataCreate);
+        return back()->with('success', 'Berhasil menambahkan data produk');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = \App\Models\Product::findOrFail($id);
+        return view('product.show',compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = \App\Models\Product::findOrFail($id);
+        $category = \App\Models\Category::all();
+        return view('product.edit',compact('product','category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = \App\Models\Product::findOrFail($id);
+        $input = $request->all();
+
+        $dataValidator = [
+            'category_id' => 'required|numeric',
+            'name' => 'required|string',
+            'description' => 'required|string',
+        ];
+        $validator = Validator::make($input,$dataValidator);
+        if($validator->fails()){
+            return back()->with('error', $validator->errors()->all());
+        }
+
+        $dataUpdate = [
+            'category_id' => $request->category_id,
+            'name' => $request->name,
+            'description' => $request->description,
+        ];
+        $product->update($dataUpdate);
+        return back()->with('success', 'Berhasil memperbarui data produk');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
